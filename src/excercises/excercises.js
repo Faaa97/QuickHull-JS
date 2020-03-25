58f34994a6861b54f178fb4833f1f624c16bb8d3
Felipe Andrés Álvarez Avaria <alu0100969535@ull.edu.es>
2020-03-25 13:53:21 +0000

    Ejercicios presenciales

---
+++ src/excercises/excercises.js
@@ -0,0 +1,11 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Felipe Andrés Alvarez Avaria <alu0100969535@ull.edu.es>
+ * @file canvas excercises
+ */
+
+'use_strict';
